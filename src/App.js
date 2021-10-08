2b0c75fa11a91b1c59b8052c25a8b347d3036c4f
rdperry031 <rdperry031@gmail.com>
2021-10-08 11:18:54 -0500

    completed smurfList

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import React, { Component } from "react";
 import AddForm from './components/AddForm';
 import SmurfList from './components/SmurfList';
 import Header from './components/Header';
+import { connect } from 'react-redux';
+import { fetchSmurfs } from './actions/index'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
 class App extends Component {
-
+  componentDidMount (){
+    fetchSmurfs();
+  }
   
   render() {
     return (
@@ -24,7 +28,7 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(null, fetchSmurfs)(App);
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
--- src/components/SmurfList.js
+++ src/components/SmurfList.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Smurf from './Smurf';
+import { connect } from 'react-redux';
 
- const SmurfList = ()=> {
+ const SmurfList = (props)=> {
+    const { smurfs } = props;
     const isLoading = false;
     const testSmurf = {
         id:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
@@ -16,11 +18,17 @@ import Smurf from './Smurf';
     }
 
     return(<div className="listContainer">
-        <Smurf smurf={testSmurf}/>
+      {smurfs.map(smurf => <Smurf key={smurfs.id} smurf={smurf}/>)}
     </div>);
 }
 
-export default SmurfList;
+const mapStateToProps = (state) => {
+    return({
+        smurfs: state.smurfs
+    })
+}
+
+export default connect(mapStateToProps)(SmurfList);
 
 //Task List:
 //1. Connect the smurfs and loading state values to the SmurfList component.
